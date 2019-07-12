1d10111c8aa76ac35262c67261ce4ea8fb798b01
Femi Odungtan <d34kon@D34kons-iMac.local>
2019-07-11 20:58:23 -0700

    setup of the express js server

---
+++ server/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly",
+        "require": "writable",
+        "process": "writable"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "plugins": [
+        "vue",
+    ],
+    "rules": {
+    }
+};
